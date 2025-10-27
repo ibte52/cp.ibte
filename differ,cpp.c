0eaadcbab925199450c1e1a62e35034cd4f954fc
Ibtesam Hoassin <ibtesamhossain52@gmail.com>
2025-10-27 21:12:36 +0600

    Add files via upload

---
+++ differ,cpp.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+    long long result = (A * B) - (C * D);
+    cout << "Difference = " << result << endl;
+    return 0;
+}
